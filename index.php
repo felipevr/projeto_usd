84ac159c547f5486d13bbc6fdd575990cf7bf817
Felipe V. Rigo <github@felipevr.eti.br>
2015-10-21 08:42:17 -0300

    Adicionado comentário

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
+//esta linha mostra o hello world
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
 }); 
