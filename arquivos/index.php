6c9a41ffe5931ebf8f69b9e60bb446ae9ba7fd65
Felipe V. Rigo <github@felipevr.eti.br>
2017-02-01 16:20:48 -0300

    adicionado mais arquivos

---
+++ arquivos/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php'; 
+
+$app = new Silex\Application(); 
+
+//esta linha mostra o hello world
+$app->get('/hello/{name}', function($name) use($app) { 
+    return 'Hello '.$app->escape($name); 
+}); 
+
+$app->run();
