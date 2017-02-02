9e2426bb48dad98fbef1f2d8535203b692246027
Felipe V. Rigo <github@felipevr.eti.br>
2017-02-02 14:11:27 -0300

    consertei o endereço de email

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
+// endereço de email
+// bla bla bla
+// suporte.nti@ufms.br
+
 //esta linha mostra o hello world
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
