effc1f04cacd29464048ac2779e101d7107c34d3
Felipe V. Rigo <felipevr@gmail.com>
2019-04-02 22:04:19 -0400

    desafios modulo 02

--- modulo02/index.js
+++ modulo02/index.js
@@ -1,9 +1,25 @@
 const readline = require('readline-sync');
+const { linkType, get } = require("get-content");
 
-const url = readline.question("Entre com a URL desejada:");
+
+let url = readline.question("Entre com a URL desejada: ");
+
+let type = linkType(url);
+
+if (!type) {
+    url = 'http://'+url;
+    type = linkType(url);
+}
+
 
 console.log(url);
 
 console.log("Estamos processando...");
 
+console.log("tipo: " + type);
 
+get(url).then((pageContent) => {
+    console.log(pageContent); // <html>\n\t<head>\n...'
+  }).catch((err) => {
+    console.warn(err); // Something happen !
+  });
