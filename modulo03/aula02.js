b53b82eda3d2ae9238b7a26328aabba467035400
Felipe V. Rigo <felipevr@gmail.com>
2019-04-06 21:11:53 -0400

    modulo 03

--- modulo03/aula02.js
+++ modulo03/aula02.js
@@ -2,6 +2,9 @@ let x = 7
 let y = 2
 
 console.log('X + Y = ' + (x + y))
-console.log('X + Y = ' + (x - y))
-console.log('X + Y = ' + (x * y))
-console.log('X + Y = ' + (x / y))
+console.log('X - Y = ' + (x - y))
+console.log('X * Y = ' + (x * y))
+console.log('X / Y = ' + (x / y))
+
+
+console.log('X % Y = ' + (x % y))
---
+++ modulo03/aula04.js
@@ -0,0 +1,10 @@
+let x = 7
+let y = 2
+
+console.log('X % Y = ' + (x % y))
+
+console.log(y)
+console.log(++y)
+console.log(y)
+console.log(y++)
+console.log(y)
