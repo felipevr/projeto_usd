0432909ababb1c5d1691c264ba5f4f828b584c17
Felipe V. Rigo <felipevr@gmail.com>
2019-04-20 16:47:12 -0400

    projeto prático - Organizar o código em funções

--- projeto_pratico/index.js
+++ projeto_pratico/index.js
@@ -2,29 +2,58 @@ const readline = require('readline-sync');
 const axios = require('axios');
 const { linkType, get } = require("get-content");
 
-const xpression = /((www\.|(http|https)+\:\/\/)[_.a-z0-9-]+\.[a-z0-9\/_:@=.+?,##%&~-]*[^.|\'|\# |!|\(|?|,| |>|<|;|\)])/gi
-const regex = new RegExp(xpression);
 
-let url = readline.question("Entre com a URL desejada: ");
 
-if(url.match(regex)) {
-  console.log("Estamos processando...");
-  axios.get(url, {responseType: 'text'})
-  .then(function (result){
-    console.log(result);
-  })
-  ;
-} else {
-  console.log('A URL informada não é válida');
-  return;
-}
+
+const urlReader = function () {
+
+    const xpression = /((www\.|(http|https)+\:\/\/)[_.a-z0-9-]+\.[a-z0-9\/_:@=.+?,##%&~-]*[^.|\'|\# |!|\(|?|,| |>|<|;|\)])/gi
+    const regex = new RegExp(xpression);
+
+    let url = readline.question("Entre com a URL desejada: ");
+
+    return url.match(regex) ? url : null;
+
+};
+
+const downloadPage = function (url, callback) {
+
+    axios.get(url, { responseType: 'text' })
+        .then((result) => callback(null, result))
+        .catch((err) => callback(err, null))
+        ;
+
+};
+
+const main = function () {
+
+    const url = urlReader();
+
+    if (!url) {
+        console.log('A URL informada não é válida');
+        return;
+    }
+
+    console.log("Estamos processando...");
+
+    downloadPage(url, (err, page) => {
+        if(err) {
+            console.error(err);
+        }
+    });
+
+
+};
+
+
+main();
 
 return;
 
 let type = linkType(url);
 
 if (!type) {
-    url = 'http://'+url;
+    url = 'http://' + url;
     type = linkType(url);
 }
 
@@ -36,6 +65,6 @@ console.log("tipo: " + type);
 
 get(url).then((pageContent) => {
     console.log(pageContent); // <html>\n\t<head>\n...'
-  }).catch((err) => {
+}).catch((err) => {
     console.warn(err); // Something happen !
-  });
+});
