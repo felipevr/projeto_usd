a1988adf9815c389f91de60c227e26f07ee40203
Felipe V. Rigo <felipevr@gmail.com>
2019-04-20 22:23:20 -0400

    modulo 04 - projeto pratico - usando cheerio

--- projeto_pratico/bots/botScrapingOG.js
+++ projeto_pratico/bots/botScrapingOG.js
@@ -1,10 +1,24 @@
+const cheerio = require('cheerio');
 
+const readMetaTag = function($, meta) {
+    return $(`meta[property="og:${meta}"]`).attr('content');
+
+};
 
 const scrapingOG = function (err, page) {
     if(err) {
         console.error(err);
     }
 
+    const $ = cheerio.load(page.data);
+    var title = readMetaTag($, "title");
+    var type = readMetaTag($, "type");
+    var image = readMetaTag($, "image");
+    var url = readMetaTag($, "url");
+    console.log(title);
+    console.log(type);
+    console.log(image);
+    console.log(url);
 };
 
 
