e775f2f56aa38ac0ac0ce557fe54ada9bd32ed85
Felipe V. Rigo <felipevr@gmail.com>
2019-07-16 21:57:26 -0400

    modulo 09 - projeto pratico

---
+++ curso/projeto_pratico/public/js/PagesController.js
@@ -0,0 +1,6 @@
+class PagesController {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+    }
+}
---
+++ curso/projeto_pratico/public/js/PagesView.js
@@ -0,0 +1,6 @@
+class PagesModel {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+    }
+}
