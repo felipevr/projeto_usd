bb3f687043d8cd21de42c31851ee952bc39fd934
Felipe V. Rigo <felipevr@gmail.com>
2019-07-20 07:21:44 -0400

    Olá mundo - projeto USD!

---
+++ projeto_usd/CotacaoSchema.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const cotacaoSchema = new mongoose.Schema({
+    valor: {type: Number, required: true},
+    data: {type: Date, required: true}
+});
+
+exports = module.exports = mongoose.model('Cotacao', cotacaoSchema, 'cotacao');
--- projeto_usd/server.js
+++ projeto_usd/server.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 
 //contem apenas a string de conexão com o https://cloud.mongodb.com
 const connectionString = require('./connectionString');
 
-
-console.log(connectionString);
-
-
 mongoose.connect(connectionString, {
     useNewUrlParser: true
 });
 
+const app = express();
+
+const Cotacao = require('./CotacaoSchema');
+
+app.use(bodyParser.json());
 
 
+app.get('/', (req, res) => {
+    Cotacao.findOne().sort('-data').exec((err, res) => {
+        console.log(err, res);
+    });
+    return res.status(200).json('Olá mundo!');
+});
 
+app.listen(3030);
+
+exports = module.exports = app
