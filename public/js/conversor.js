b4348a68e781957b219bba1a6199daef8c108f03
Felipe V. Rigo <felipevr@gmail.com>
2019-08-04 22:37:54 -0400

    Programação do Frontend ok

--- public/js/conversor.js
+++ public/js/conversor.js
@@ -12,9 +12,44 @@ window.addEventListener('load', event => {
     ;
 });
 
+document.getElementById('btnInverter').addEventListener('click', inverter);
+document.forms['conversor'].addEventListener('submit', converter);
+
 function atualizarTela(cotacao) {
     document.getElementById('cotacao-data').innerText = moment(cotacao.data).format('DD/MM/YYYY');
     document.getElementById('cotacao-venda').innerText = cotacao.venda;
     document.getElementById('cotacao-compra').innerText = cotacao.compra;
-    console.log(cotacao);
+}
+
+function inverter() {
+    let entrada = document.querySelector('label[for="inputEntrada"] strong').innerText;
+    let saida = document.querySelector('label[for="inputResultado"] strong').innerText;
+    
+    document.querySelector('label[for="inputEntrada"] strong').innerText = saida;
+    document.querySelector('label[for="inputResultado"] strong').innerText = entrada;
+
+    invertido = !invertido;
+}
+
+function converter(event) {
+    event.preventDefault();
+    let entrada = document.querySelector('#inputEntrada').value;
+
+    if (!entrada) {
+        return;
+    }
+
+    entrada = parseFloat(entrada.replace('.', '').replace(',', '')) / 100.0;
+
+    let operacao = Array.from(document.querySelectorAll('input[name="operacao"]'))
+        .find(radio => radio.checked).value;
+
+    let vlCotacao = operacao == "compra" ? cotacao.compra : cotacao.venda;
+
+    if (invertido) {
+        vlCotacao = 1 / vlCotacao;
+    }
+
+    let resultado = parseFloat((entrada / vlCotacao).toFixed(2)).toLocaleString('pt-BR');
+    document.querySelector('#inputResultado').value = resultado;
 }
